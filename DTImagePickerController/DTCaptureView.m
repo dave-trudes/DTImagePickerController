44e6ba2bda9ee30ce737ad9082ac9148387858d9
dave <david@iwa-tech.com>
2012-10-31 09:45:28 +0100

    Overridden initWithCoder method

--- DTImagePickerController/DTCaptureView.m
+++ DTImagePickerController/DTCaptureView.m
@@ -42,6 +42,15 @@
 	return self;
 }
 
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+	self = [super initWithCoder:aDecoder];
+	if (self) {
+		[self setup];
+	}
+	return self;
+}
+
 - (void)setup
 {
 	//Setting defaults
